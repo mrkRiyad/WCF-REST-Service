2208fd001be7d5232fdef15e10ef40db5fc5ca5d
mrkRiyad <dev.riyad@gmail.com>
2018-11-19 22:38:36 +0600

    Added Entity Framework code first model

---
+++ WcfService1/Models/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfService1.Models
+{
+    public class Library : DbContext
+    {
+        public DbSet<Book> Books { get; set; }  
+    }
+
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Author { get; set; }
+    }
+}
